1bd85188a5bf955b286ab333826dec16bb5fb25e
deadash <dead.ash@qq.com>
2020-03-08 21:28:14 +0800

    谷歌插件

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	// crack pro
+	$('#components-menu__logo').prop('id', 'components-menu__user-name');
+	// crack for customizer theme
+	$('.customizer__pro').remove();
+	// remove all 
+	$('.unlock-all-items').remove();
+});
